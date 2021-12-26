eff5260c20eba107d17a2f0bbf65fed8711d6b44
Hiroki mac air <aichan_lovepower@yahoo.co.jp>
2021-12-26 18:35:36 +0900

    Initial Commit

---
+++ Shared/TokuMoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokuMoApp.swift
+//  Shared
+//
+//  Created by 林宏樹 on 2021/12/26.
+//
+
+import SwiftUI
+
+@main
+struct TokuMoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
