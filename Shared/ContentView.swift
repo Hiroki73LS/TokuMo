f02ef0b84f9b46802950b5ed0af1762dbae47837
Hiroki mac air <aichan_lovepower@yahoo.co.jp>
2021-12-30 18:35:49 +0900

    広告のサイズを変更

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,7 +7,7 @@ struct BannerAdView: UIViewRepresentable {
 
     func makeUIView(context: Context) -> GADBannerView {
         
-        let banner = GADBannerView(adSize: kGADAdSizeBanner)
+        let banner = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
         banner.adUnitID = "ca-app-pub-1023155372875273/2406169933"
         //↑こちら本物。
 //        banner.adUnitID = "ca-app-pub-3940256099942544/2934735716"
